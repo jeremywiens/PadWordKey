e505dd9313350b6462f6ddb6a1c89e058c8432df
jeremywiens <jeremydwiens@gmail.com>
2018-01-22 21:10:29 -0800

    Completed/ fixed main

--- src/main/java/words/Main.java
+++ src/main/java/words/Main.java
@@ -3,17 +3,22 @@ package words;
 import java.io.IOException;
 import java.util.*;
 
+import static java.lang.Character.isLetter;
+
 public class Main {
 
-        public static void main(String[] args){
-            Scanner input = new Scanner(System.in);
-            boolean quit = false;
-            while(!quit) {
-                System.out.println("Enter your number to find the possible words: ");
-                String number = input.nextLine();
-                if(number.toLowerCase().equals("quit"))
-                    quit = true;
-                else {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean quit = false;
+        while (!quit) {
+            System.out.println("Enter your number to find the possible words or \nenter a word to find the number: ");
+            String number = input.nextLine();
+            if (number.toLowerCase().equals("quit"))
+                quit = true;
+            else {
+                if (isLetter(number.charAt(0))) {
+                    System.out.println(WordToNumber.getNumber(number));
+                } else {
                     List<String> words = null;
                     try {
                         words = findActualWords(number);
@@ -24,22 +29,19 @@ public class Main {
                 }
             }
         }
-
-        public static List<String> findActualWords(String number) throws IOException{
-            HashMap<Integer, List<String>> listWords = WordLengths.createWordMap();
-            List<String> WordsOfLength = listWords.get(number.length());
-            List<String> PossibleWords = WordPermutations.possibleWords(number);
-            List<String> RealWords = new ArrayList<>();
-            for(String s: WordsOfLength)
-            {
-                for(String k: PossibleWords){
-                    if(s.equals(k)){
-                        RealWords.add(s);
-                    }
-                }
-            }
-            return RealWords;
-        }
     }
+
+    public static List<String> findActualWords(String number) throws IOException {
+        HashMap<Integer, List<String>> listWords = WordLengths.createWordMap();
+        List<String> WordsOfLength = listWords.get(number.length());
+        List<String> PossibleWords = WordPermutations.possibleWords(number);
+        List<String> RealWords = new ArrayList<>();
+        for (String s : WordsOfLength) {
+            if (PossibleWords.contains(s))
+                RealWords.add(s);
+        }
+        return RealWords;
+    }
+}
 
 
